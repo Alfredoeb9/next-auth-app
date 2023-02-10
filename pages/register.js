97e8814dfe960916e1167ce55a9b992adaaebd84
Alfredoeb9 <alfredoeb96@gmail.com>
2023-02-09 19:56:50 -0800

    [register] created register page

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,92 @@
 import Head from "next/head";
+import Layout from "@/layout/layout";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Form.module.css";
+import { HiAtSymbol, HiFingerPrint, HiOutlineUser } from "react-icons/hi";
+import { useState } from "react";
 
 function Register() {
+  const [show, setShow] = useState({ password: false, cpassword: false });
   return (
-    <div>
+    <Layout>
       <Head>
         <title>Register</title>
       </Head>
-      <h1>Login</h1>
-    </div>
+      <section className="w-3/4 mx-auto flex flex-col gap-10">
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">Register</h1>
+          <p className="w-3/4 mx-auto text-gray-400">Login in Today!</p>
+        </div>
+
+        <form className="flex flex-col gap-5">
+          <div className={styles.input_group}>
+            <input
+              type="text"
+              name="Username"
+              placeholder="Username"
+              className={styles.input_text}
+            />
+            <span className="icon flex items-center px-4">
+              <HiOutlineUser size={25} />
+            </span>
+          </div>
+          <div className={styles.input_group}>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              className={styles.input_text}
+            />
+            <span className="icon flex items-center px-4">
+              <HiAtSymbol size={25} />
+            </span>
+          </div>
+          <div className={styles.input_group}>
+            <input
+              type={`${show.password ? "text" : "password"}`}
+              name="password"
+              placeholder="password"
+              className={styles.input_text}
+            />
+            <span
+              className="icon flex items-center px-4"
+              onClick={() => setShow({ ...show, password: !show.password })}
+            >
+              <HiFingerPrint size={25} />
+            </span>
+          </div>
+
+          <div className={styles.input_group}>
+            <input
+              type={`${show.cpassword ? "text" : "password"}`}
+              name="cpassword"
+              placeholder="Confirm Password"
+              className={styles.input_text}
+            />
+            <span
+              className="icon flex items-center px-4"
+              onClick={() => setShow({ ...show, cpassword: !show.cpassword })}
+            >
+              <HiFingerPrint size={25} />
+            </span>
+          </div>
+
+          <div className="input-button">
+            <button type="submit" className={styles.button}>
+              Login
+            </button>
+          </div>
+        </form>
+
+        <p className="text-center text-gray-400 ">
+          Have an Account?{" "}
+          <Link href={"/login"} className="hover:text-black">
+            Sign In
+          </Link>
+        </p>
+      </section>
+    </Layout>
   );
 }
 
