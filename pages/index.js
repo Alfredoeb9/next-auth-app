1b60de82bb3e8293073d99e0d4c870ecfd59c5b2
Alfredoeb9 <alfredoeb96@gmail.com>
2023-02-09 20:18:54 -0800

    [pages] created home and guest page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [session, setSession] = useState(false);
   return (
     <>
       <Head>
@@ -14,9 +17,52 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className="text-3xl font-bold underline">Holle World</div>
-      </main>
+      {session ? AuthUser() : Guest()}
     </>
+  );
+}
+
+function Guest() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-4xl font-bold underline">Guest Homepage</h3>
+
+      <div className="flex justify-center">
+        <Link
+          href={"/login"}
+          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-white"
+        >
+          Sign In
+        </Link>
+      </div>
+    </main>
+  );
+}
+
+function AuthUser() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-4xl font-bold underline">Authorized User Homepage</h3>
+
+      <div className="details">
+        <h5>Unknown</h5>
+        <h5>Unknown</h5>
+      </div>
+
+      <div className="flex justify-center">
+        <button className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-white">
+          Sign Out
+        </button>
+      </div>
+
+      <div className="flex justify-center">
+        <Link
+          href={"/profile"}
+          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-white"
+        >
+          Profile
+        </Link>
+      </div>
+    </main>
   );
 }
