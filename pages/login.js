34bc16d92e9710d032e559e4e569e845d7042831
Alfredoeb9 <alfredoeb96@gmail.com>
2023-02-09 18:23:52 -0800

    [app] brought in tailwind and fonts, setup initial login screen and assets

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Layout from "@/layout/layout";
+
+function Login() {
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Login</h1>
+    </Layout>
+  );
+}
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Register() {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default Register;
